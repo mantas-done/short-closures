e0122083741fcedc698367cc93422a39d7e58308
mantas-done <mantas783@gmail.com>
2017-12-17 14:58:41 +0200

    Remove unneeded param

--- src/helpers.php
+++ src/helpers.php
@@ -2,6 +2,6 @@
 
 if (!function_exists('c')) {
     function c($code) {
-        return \MantasDone\ShortClosures\ShortClosure::generate($code, $params = []);
+        return \MantasDone\ShortClosures\ShortClosure::generate($code);
     }
 }
